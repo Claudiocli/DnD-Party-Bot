b721e9da6db980f36dac8cded4c4dfb651d393a3
Claudiocli <claudiocli.cp@gmail.com>
2025-06-24 15:33:38 +0200

    Fix to made visible categories to everyone

--- commands/create_campaign.py
+++ commands/create_campaign.py
@@ -93,12 +93,14 @@ class CreateCampaign(commands.Cog):
                 category = await guild.create_category(name=name)
                 await category.set_permissions(role_dm, view_channel=True, mute_members=True)
                 await category.set_permissions(role_pl, view_channel=True)
-                await category.set_permissions(role_e, view_channel=True)
+                await category.set_permissions(role_e, view_channel=False)
 
                 await guild.create_stage_channel(name=f"{name}_vocal", category=category)
                 await guild.create_text_channel(name=f"{name}_text", category=category)
                 await guild.create_text_channel(name=f"{name}_meme", category=category)
                 await guild.create_text_channel(name=f"{name}_organize", category=category)
+                # To make everyone only see the stage channel
+                await guild.get_channel(name=f"{name}_vocal").set_permissions(role_e, view_channel=True)
 
                 await member.add_roles(role_dm)
 
